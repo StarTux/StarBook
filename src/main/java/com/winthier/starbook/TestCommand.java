f161aa20654c0e6e59e74444300ea32d131aea41
StarTux <ths.loy@googlemail.com>
2017-04-18 11:00:24 +0200

    Clear this

--- src/main/java/com/winthier/starbook/TestCommand.java
+++ src/main/java/com/winthier/starbook/TestCommand.java
@@ -6,14 +6,5 @@ import org.bukkit.entity.EntityType;
 class TestCommand extends AbstractCommand {
     @Override
     public void onCommand(CommandContext c) {
-        if (c.args.length != 2) return;
-        EntityType entityType = EntityType.valueOf(c.args[0].toUpperCase());
-        int radius = Integer.parseInt(c.args[1]);
-        int count = 0;
-        double r = (double)radius;
-        for (Entity e: c.player.getNearbyEntities(r, r, r)) {
-            if (e.getType() == entityType) count += 1;
-        }
-        c.player.sendMessage("Count " + entityType + ": " + count);
     }
 }
