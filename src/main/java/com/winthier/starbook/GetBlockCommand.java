0df4c003ba30930716f2e692cf0fc2132ef21d2d
StarTux <ths.loy@googlemail.com>
2017-07-14 14:15:41 +0200

    Show light levels

--- src/main/java/com/winthier/starbook/GetBlockCommand.java
+++ src/main/java/com/winthier/starbook/GetBlockCommand.java
@@ -12,6 +12,6 @@ class GetBlockCommand extends AbstractCommand {
         if (c.player == null) StarBookCommandException.playerExpected();
         Block block = c.player.getLocation().getBlock();
         int data = (int)block.getData();
-        msg(c.player, "%s:%d (%d:%d) [%s]", block.getType(), data, block.getType().getId(), data, Integer.toBinaryString(data));
+        msg(c.player, "%s:%d (%d:%d[%s]) light=%d (sky=%d block=%d)", block.getType(), data, block.getType().getId(), data, Integer.toBinaryString(data), block.getLightLevel(), block.getLightFromSky(), block.getLightFromBlocks());
     }
 }
