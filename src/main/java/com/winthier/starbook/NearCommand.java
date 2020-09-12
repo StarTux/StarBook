173631dad0063d5bea4a93268b86c9e38dd37d70
StarTux <ths.loy@googlemail.com>
2020-09-13 01:55:03 +0200

    Show direction

--- src/main/java/com/winthier/starbook/NearCommand.java
+++ src/main/java/com/winthier/starbook/NearCommand.java
@@ -3,7 +3,6 @@ package com.winthier.starbook;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
-import lombok.RequiredArgsConstructor;
 import org.bukkit.Location;
 import org.bukkit.entity.Player;
 
@@ -15,19 +14,39 @@ class NearCommand extends AbstractCommand {
         Location loc = c.player.getLocation();
         for (Player player: c.player.getWorld().getPlayers()) {
             if (player.equals(c.player)) continue;
-            proxies.add(new Prox(player.getName(), loc.distance(player.getLocation())));
+            proxies.add(new Prox(loc, player));
         }
         Collections.sort(proxies);
         for (Prox prox: proxies) {
-            msg(c.player, "&e%d&r %s", (int)prox.dist, prox.name);
+            msg(c.player, "&e%d&r %s &7%s", (int) prox.dist, prox.name, prox.dir);
         }
     }
 }
 
-@RequiredArgsConstructor
 class Prox implements Comparable<Prox> {
     final String name;
     final double dist;
+    final String dir;
+
+    Prox(final Location center, final Player player) {
+        Location loc = player.getLocation();
+        this.name = player.getName();
+        this.dist = loc.distance(loc);
+        StringBuilder sb = new StringBuilder();
+        double dz = loc.getZ() - center.getZ();
+        if (Math.abs(dz) >= 1.0) {
+            sb.append(dz < 0 ? "N" : "S");
+        }
+        double dx = loc.getX() - center.getX();
+        if (Math.abs(dx) >= 1.0) {
+            sb.append(dx < 0 ? "W" : "E");
+        }
+        double dy = loc.getY() - center.getY();
+        if (Math.abs(dy) > 0) {
+            sb.append(dy < 0 ? " below" : " above");
+        }
+        this.dir = sb.toString();
+    }
 
     @Override
     public int compareTo(Prox o) {
