3f8414287352cce2e35c680c44ac4c5eeb2671e7
StarTux <ths.loy@googlemail.com>
2020-10-11 14:36:51 +0200

    Update lombok and WorldEdit

--- src/main/java/com/winthier/starbook/WorldEditHighlightCommand.java
+++ src/main/java/com/winthier/starbook/WorldEditHighlightCommand.java
@@ -1,10 +1,13 @@
 package com.winthier.starbook;
 
+import com.sk89q.worldedit.LocalSession;
 import com.sk89q.worldedit.bukkit.WorldEditPlugin;
-import com.sk89q.worldedit.bukkit.selections.Selection;
+import com.sk89q.worldedit.math.BlockVector3;
+import com.sk89q.worldedit.regions.Region;
 import lombok.RequiredArgsConstructor;
 import org.bukkit.Bukkit;
 import org.bukkit.Particle;
+import org.bukkit.World;
 import org.bukkit.block.Block;
 import org.bukkit.entity.Player;
 import org.bukkit.scheduler.BukkitRunnable;
@@ -17,16 +20,34 @@ final class WorldEditHighlightCommand extends AbstractCommand {
     public void onCommand(CommandContext c) {
         if (c.player == null) StarBookCommandException.playerExpected();
         WorldEditPlugin we = getWorldEdit();
-        if (we == null) throw new StarBookCommandException("WorldEdit not foundx!");
-        Selection sel = we.getSelection(c.player);
+        if (we == null) throw new StarBookCommandException("WorldEdit not found!");
+        Cuboid sel = getSelection(c.player);
         if (sel == null) throw new StarBookCommandException("Make a selection first!");
-        Block a = sel.getMinimumPoint().getBlock();
-        Block b = sel.getMaximumPoint().getBlock();
+        World w = c.player.getWorld();
+        Block a = sel.getMinBlock(w);
+        Block b = sel.getMaxBlock(w);
         new HighlightTask(c.player, a, b).runTaskTimer(plugin, 1, 1);
     }
 
-    WorldEditPlugin getWorldEdit() {
+    public static WorldEditPlugin getWorldEdit() {
         return (WorldEditPlugin) Bukkit.getServer().getPluginManager().getPlugin("WorldEdit");
+    }
+
+    public static Cuboid getSelection(Player player) {
+        WorldEditPlugin we = getWorldEdit();
+        LocalSession session = we.getSession(player);
+        com.sk89q.worldedit.world.World world = session.getSelectionWorld();
+        final Region region;
+        try {
+            region = session.getSelection(world);
+        } catch (Exception e) {
+            return null;
+        }
+        if (region == null) return null;
+        BlockVector3 min = region.getMinimumPoint();
+        BlockVector3 max = region.getMaximumPoint();
+        return new Cuboid(min.getBlockX(), min.getBlockY(), min.getBlockZ(),
+                          max.getBlockX(), max.getBlockY(), max.getBlockZ());
     }
 
     @RequiredArgsConstructor
@@ -38,7 +59,7 @@ final class WorldEditHighlightCommand extends AbstractCommand {
         private int count = 0;
         @Override
         public void run() {
-            if (!player.isValid()) {
+            if (!player.isValid() || !player.getWorld().equals(a.getWorld())) {
                 cancel();
                 return;
             }
