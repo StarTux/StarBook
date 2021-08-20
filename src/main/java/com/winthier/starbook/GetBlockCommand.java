1d6c2b66b6c8c68bd0e35556377f13ed1c11451b
StarTux <ths.loy@googlemail.com>
2021-08-20 14:02:14 +0200

    Remove Dirty and Msg

--- src/main/java/com/winthier/starbook/GetBlockCommand.java
+++ src/main/java/com/winthier/starbook/GetBlockCommand.java
@@ -1,9 +1,7 @@
 package com.winthier.starbook;
 
-import com.cavetale.dirty.Dirty;
 import lombok.RequiredArgsConstructor;
 import org.bukkit.block.Block;
-import org.json.simple.JSONValue;
 
 @RequiredArgsConstructor
 class GetBlockCommand extends AbstractCommand {
@@ -13,9 +11,7 @@ class GetBlockCommand extends AbstractCommand {
     public void onCommand(CommandContext c) {
         if (c.player == null) StarBookCommandException.playerExpected();
         Block block = c.player.getLocation().getBlock();
-        Object json = Dirty.getBlockTag(block);
-        String jsonstr = json == null ? "{}" : JSONValue.toJSONString(json);
-        msg(c.player, "%s %s l=%d (sky=%d b=%d) t=%.2f", block.getBlockData().getAsString(), jsonstr,
+        msg(c.player, "%s l=%d (sky=%d b=%d) t=%.2f", block.getBlockData().getAsString(),
             block.getLightLevel(), block.getLightFromSky(), block.getLightFromBlocks(), block.getTemperature());
     }
 }
--- src/main/java/com/winthier/starbook/WhoCommand.java
+++ src/main/java/com/winthier/starbook/WhoCommand.java
@@ -9,8 +9,12 @@ import java.util.List;
 import java.util.Map;
 import java.util.UUID;
 import lombok.RequiredArgsConstructor;
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.TextComponent;
+import net.kyori.adventure.text.event.ClickEvent;
+import net.kyori.adventure.text.event.HoverEvent;
+import net.kyori.adventure.text.format.NamedTextColor;
 import org.bukkit.Bukkit;
-import org.bukkit.ChatColor;
 import org.bukkit.GameMode;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
@@ -32,68 +36,50 @@ final class WhoCommand extends AbstractCommand {
     }
 
     void showOnlineList(CommandSender sender) {
-        if (plugin.getServer().getPluginManager().getPlugin("Connect") == null) {
-            StringBuilder sb = new StringBuilder("" + plugin.getServer().getOnlinePlayers().size() + " Online Players:");
-            for (Player player : plugin.getServer().getOnlinePlayers()) sb.append(" ").append(player.getName());
-            sender.sendMessage(sb.toString());
-            return;
-        }
         Map<String, List<OnlinePlayer>> serverList = Connect.getInstance().listPlayers();
         List<String> serverNames = new ArrayList<>(serverList.keySet());
         Collections.sort(serverNames, (a, b) -> Integer.compare(serverList.get(b).size(), serverList.get(a).size()));
-        if (sender instanceof Player) {
-            Player player = (Player) sender;
-            int totalCount = 0;
-            List<List<Object>> msgs = new ArrayList<>();
-            for (String serverName : serverNames) {
-                List<OnlinePlayer> playerList = new ArrayList<>(serverList.get(serverName));
+        int totalCount = 0;
+        List<Component> lines = new ArrayList<>();
+        for (String serverName : serverNames) {
+            List<OnlinePlayer> playerList = new ArrayList<>(serverList.get(serverName));
+            if (sender instanceof Player) {
                 playerList.removeIf(it -> {
                         Player online = Bukkit.getPlayer(it.getUuid());
-                        return online != null && ((online.getGameMode() == GameMode.SPECTATOR && online.hasPermission("chat.invisible")) || !player.canSee(online));
+                        return online != null && online.getGameMode() == GameMode.SPECTATOR && online.hasPermission("chat.invisible");
                     });
-                serverList.put(serverName, playerList);
-                String perm = "starbook.server." + serverName.toLowerCase();
-                if (playerList.size() == 0) continue;
-                Collections.sort(playerList, (a, b) -> String.CASE_INSENSITIVE_ORDER.compare(a.getName(), b.getName()));
-                List<Object> json = new ArrayList<>();
-                msgs.add(json);
-                totalCount += playerList.size();
-                json.add(" ");
-                if (player.isPermissionSet(perm) && player.hasPermission(perm)) {
-                    json.add(Msg.button(ChatColor.BLUE,
-                                        Msg.format("&7%s(&f%d&7)", serverName, playerList.size()),
-                                        serverName + " Server\n&7&o/" + serverName.toLowerCase(),
-                                        "/" + serverName.toLowerCase() + " "));
-                } else {
-                    json.add(Msg.button(ChatColor.BLUE,
-                                        Msg.format("&7%s(&f%d&7)", serverName, playerList.size()),
-                                        serverName + " Server",
-                                        null));
-                }
-                for (OnlinePlayer online : playerList) {
-                    json.add(" ");
-                    if (isStaff(online.getUuid())) {
-                        json.add(Msg.button(ChatColor.GOLD,
-                                            online.getName(),
-                                            "&6" + online.getName() + " &r&oStaff\n&7&o/msg " + online.getName(),
-                                            "/msg " + online.getName() + " "));
-                    } else {
-                        json.add(Msg.button(ChatColor.WHITE,
-                                            online.getName(),
-                                            online.getName() + "\n&7&o/msg " + online.getName(),
-                                            "/msg " + online.getName() + " "));
-                    }
-                }
             }
-            Msg.msg(player, "&9%s Player List (&f%d&9)", Connect.getInstance().getServerName(), totalCount);
-            for (List<Object> json : msgs) Msg.raw(player, json);
-        } else {
-            for (String serverName : serverNames) {
-                OnlinePlayer[] playerArray = serverList.get(serverName).toArray(new OnlinePlayer[0]);
-                StringBuilder sb = new StringBuilder(serverName).append("(").append(playerArray.length).append(")");
-                for (OnlinePlayer p : playerArray) sb.append(" ").append(p.getName());
-                sender.sendMessage(sb.toString());
+            serverList.put(serverName, playerList);
+            if (playerList.size() == 0) continue;
+            Collections.sort(playerList, (a, b) -> String.CASE_INSENSITIVE_ORDER.compare(a.getName(), b.getName()));
+            totalCount += playerList.size();
+            Component nameComponent = TextComponent.ofChildren(new Component[] {
+                    Component.text(serverName, NamedTextColor.GRAY),
+                    Component.text(" (", NamedTextColor.GRAY),
+                    Component.text("" + playerList.size(), NamedTextColor.WHITE),
+                    Component.text(")", NamedTextColor.GRAY),
+                })
+                .clickEvent(ClickEvent.suggestCommand("/" + serverName.toLowerCase()))
+                .hoverEvent(HoverEvent.showText(Component.text("/" + serverName.toLowerCase(), NamedTextColor.GREEN)));
+            List<Component> playerNames = new ArrayList<>();
+            for (OnlinePlayer online : playerList) {
+                boolean staff = isStaff(online.getUuid());
+                playerNames.add(Component.text(online.getName(), staff ? NamedTextColor.GOLD : NamedTextColor.WHITE)
+                                .clickEvent(ClickEvent.suggestCommand("/msg " + online.getName()))
+                                .hoverEvent(HoverEvent.showText(TextComponent.ofChildren(new Component[] {
+                                                Component.text(online.getName(), (staff ? NamedTextColor.GOLD : NamedTextColor.WHITE)),
+                                                Component.newline(),
+                                                Component.text((staff ? "Staff member" : "Player"), NamedTextColor.DARK_GRAY),
+                                                Component.newline(),
+                                                Component.text("/msg " + online.getName(), NamedTextColor.GRAY),
+                                            }))));
             }
+            lines.add(TextComponent.ofChildren(new Component[] {
+                        nameComponent,
+                        Component.space(),
+                        Component.join(Component.text(", ", NamedTextColor.GRAY), playerNames),
+                    }));
         }
+        sender.sendMessage(Component.join(Component.newline(), lines));
     }
 }
