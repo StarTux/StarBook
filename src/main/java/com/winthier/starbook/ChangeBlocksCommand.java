3d23c31384ef7bba1a15ea37221f6b80cb164058
StarTux <ths.loy@googlemail.com>
2023-02-13 13:06:49 +0100

    Allow a simple filter

--- src/main/java/com/winthier/starbook/ChangeBlocksCommand.java
+++ src/main/java/com/winthier/starbook/ChangeBlocksCommand.java
@@ -16,8 +16,21 @@ final class ChangeBlocksCommand extends AbstractCommand {
     @Override
     public void onCommand(CommandContext c) {
         if (c.player == null) throw new StarBookCommandException("[starbook:changeblocks] player expected");
-        if (c.args.length != 1) throw new StarBookCommandException(c);
-        String blockArg = c.args[0];
+        final BlockData from;
+        final String blockArg;
+        if (c.args.length == 2) {
+            try {
+                from = Bukkit.createBlockData(c.args[0]);
+            } catch (IllegalArgumentException iae) {
+                throw new StarBookCommandException("Invalid from block data: " + c.args[0]);
+            }
+            blockArg  = c.args[1];
+        } else if (c.args.length == 1) {
+            from = null;
+            blockArg = c.args[0];
+        } else {
+            throw new StarBookCommandException(c);
+        }
         final BlockData blockData;
         try {
             blockData = Bukkit.createBlockData(blockArg);
@@ -34,14 +47,17 @@ final class ChangeBlocksCommand extends AbstractCommand {
             private int z = selection.az;
             private int progress = 0;
             private long lastReport;
+            private int changedCount;
             @Override public void run() {
                 long then = System.currentTimeMillis();
                 long now;
                 do {
                     progress += 1;
                     Block block = world.getBlockAt(x, y, z);
-                    if (!block.getBlockData().matches(blockData)) {
+                    BlockData oldBlockData = block.getBlockData();
+                    if (!oldBlockData.matches(blockData) && (from == null || from.matches(oldBlockData)))  {
                         block.setBlockData(blockData, false);
+                        changedCount += 1;
                     }
                     x += 1;
                     if (x > selection.bx) {
@@ -52,9 +68,15 @@ final class ChangeBlocksCommand extends AbstractCommand {
                             y += 1;
                             if (y > selection.by) {
                                 cancel();
-                                c.player.sendMessage("ChangeBlocks done: " + total
-                                                     + " blocks changed to "
-                                                     + blockData.getAsString());
+                                if (from != null) {
+                                    c.player.sendMessage("ChangeBlocks done: " + changedCount
+                                                         + " blocks changed from " + from.getAsString()
+                                                         + " to " + blockData.getAsString());
+                                } else {
+                                    c.player.sendMessage("ChangeBlocks done: " + changedCount
+                                                         + " blocks changed to "
+                                                         + blockData.getAsString());
+                                }
                                 return;
                             }
                         }
@@ -73,9 +95,9 @@ final class ChangeBlocksCommand extends AbstractCommand {
 
     @Override
     public List<String> onTabComplete(CommandContext c) {
-        if (c.args.length != 1) return null;
+        if (c.args.length != 1 && c.args.length != 2) return null;
         List<String> result = new ArrayList<>();
-        String cmd = c.args[0].toLowerCase();
+        String cmd = c.args[c.args.length - 1].toLowerCase();
         for (Material material : Material.values()) {
             String key = material.getKey().getKey();
             if (key.contains(cmd)) {
