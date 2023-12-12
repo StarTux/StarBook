9bd3f6d97a2a70c1d398d7e404e76d7e928c0d5f
StarTux <ths.loy@gmail.com>
2023-12-12 23:01:14 +0100

    Deny on Skyblock

--- src/main/java/com/winthier/starbook/CraftCommand.java
+++ src/main/java/com/winthier/starbook/CraftCommand.java
@@ -1,6 +1,7 @@
 package com.winthier.starbook;
 
 import com.cavetale.core.command.AbstractCommand;
+import com.cavetale.core.connect.NetworkServer;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class CraftCommand extends AbstractCommand<JavaPlugin> {
@@ -10,6 +11,7 @@ public final class CraftCommand extends AbstractCommand<JavaPlugin> {
 
     @Override
     protected void onEnable() {
+        if (NetworkServer.current() == NetworkServer.SKYBLOCK) return;
         rootNode.description("Open a crafting table").denyTabCompletion()
             .playerCaller(player -> player.openWorkbench(null, true));
     }
