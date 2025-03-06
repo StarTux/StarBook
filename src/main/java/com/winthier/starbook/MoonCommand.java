da89abcb13e0ca963c641b0906208e1907707efc
StarTux <ths.loy@gmail.com>
2025-03-06 13:50:29 +0100

    Use moon phase API

--- src/main/java/com/winthier/starbook/MoonCommand.java
+++ src/main/java/com/winthier/starbook/MoonCommand.java
@@ -6,26 +6,12 @@ import static net.kyori.adventure.text.JoinConfiguration.noSeparators;
 import static net.kyori.adventure.text.format.NamedTextColor.*;
 
 class MoonCommand extends AbstractCommand {
-    final String[] phases = {
-        "Full Moon",
-        "Waning Gibbous",
-        "Last Quarter",
-        "Waning Crescent",
-        "New Moon",
-        "Waxing Crescent",
-        "First Quarter",
-        "Waxing Gibbous"
-    };
-
     @Override
     public void onCommand(CommandContext c) {
         if (c.player == null) StarBookCommandException.playerExpected();
         if (c.args.length != 0) StarBookCommandException.usage(c);
-        long time = c.player.getWorld().getFullTime();
-        long day = time / 24000;
-        int index = (int) day % phases.length;
         c.player.sendMessage(join(noSeparators(),
                                   text("Moon phase: "),
-                                  text(phases[index], GREEN)));
+                                  text(c.player.getWorld().getMoonPhase().name(), GREEN)));
     }
 }
